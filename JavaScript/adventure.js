bda1c40f80d6c2b56076594605abfca14e6e7aed
Jonas <joshuamoorhead@gmail.com>
2016-01-06 15:44:19 -0800

    adding JavaScript

---
+++ JavaScript/adventure.js
@@ -0,0 +1,11 @@
+confirm("You ready for this shit?");
+prompt("Are you an old fuck?")
+var age = prompt("Are you an old fuck?");
+if (age === no)
+{
+	console.log("Fuck off then");
+}
+else
+{
+	console.log("Let's do this shit!");
+}
