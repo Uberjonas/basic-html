f349c2c2f997c71d34598d25438ad1fcd61ef999
Jonas <joshuamoorhead@gmail.com>
2016-01-06 17:52:20 -0800

    added some files and more js

--- JavaScript/adventure.js
+++ JavaScript/adventure.js
@@ -1,5 +1,4 @@
 confirm("You ready for this shit?");
-prompt("Are you an old fuck?")
 var age = prompt("Are you an old fuck?");
 if (age === no)
 {
@@ -8,4 +7,36 @@ if (age === no)
 else
 {
 	console.log("Let's do this shit!");
+}
+{
+	console.log("Your name is Ward, and you have decided that you 
+		are gay. You wonder how many cocks you can fit into your
+		mouth.");
+}
+{
+	console.log("Suddenly, Miranda shows up and wants to know what
+		the holy fuck?");
+}
+var userAnswer = prompt("Do you really choose mouthcocks over your
+	lady wife?");
+
+if (userAnswer === "yes")
+{
+	console.log("We thought as much. You cuntstain.");
+}
+else
+{
+	console.log("Don't lie to us. Your words are meaningless. 
+		The dicks penetrate you endlessly.")
+}
+var feedback = prompt("Rate this shit. 1-10, bitch!");
+
+if (feedback > 8)
+{
+	console.log("Damn right! This is impressive as fuck!");
+}
+else
+{
+	console.log("Go fuck yourself then. It's my JavaScript and I
+		like it.");
 }
